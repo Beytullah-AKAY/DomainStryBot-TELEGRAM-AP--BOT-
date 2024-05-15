9a68d2d489bb244a7eab867bc48061f2edc910a6
Beytullah AKAY <larkvinanemo@gmail.com>
2024-05-15 21:38:50 +0300

    Update index.js

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const domainHistory = require('./domain'); // Dosya adını düzeltin
+const domainHistory = require('./domain'); 
 const domainStatusHistory = require('./DomainStatus');
 
 router.use('/domain-history', domainHistory);
-router.use('/domainstatus-history', domainStatusHistory); // `/domainstatus-history` olarak değiştirildi
+router.use('/domainstatus-history', domainStatusHistory); 
 
 module.exports = router;
